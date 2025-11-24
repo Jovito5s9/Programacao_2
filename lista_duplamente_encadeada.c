d2536940ced1909ae72e9773baa85c30ec6e517e
Jovito5s9 <sjovito70@gmail.com>
2025-11-24 19:42:01 -0300

    declarando função de adição de novos nós

--- lista_duplamente_encadeada.c
+++ lista_duplamente_encadeada.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
-	struct no *anterior;
-	struct no *proximo;
+typedef struct no_base{
+	struct no_base *anterior;
+	struct no_base *proximo;
 	int valor;
-	} no;
+	} no_base;
+
+void adicionar_no(no_base **cabeca, int valor){
+	no_base *novo_no = malloc(sizeof(no_base));
+	novo_no->valor=valor;
+	no_base *cabeca_antiga=cabeca;
+	cabeca=&novo_no;
+	novo_no->proximo=cabeca_antiga;
+}
 
 int main() {
     
