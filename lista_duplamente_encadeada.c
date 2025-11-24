12200090d21841254e321fc2b3b87f9f69b42084
Jovito5s9 <sjovito70@gmail.com>
2025-11-24 20:07:41 -0300

    adição de função que printa a lista(ainda falta corrigir algum parametros de quebra de loop de printagem)

--- lista_duplamente_encadeada.c
+++ lista_duplamente_encadeada.c
@@ -7,15 +7,27 @@ typedef struct no_base{
 	int valor;
 	} no_base;
 
-void adicionar_no(no_base **cabeca, int valor){
+void add_no(no_base **cabeca, int valor){
 	no_base *novo_no = malloc(sizeof(no_base));
 	novo_no->valor=valor;
-	no_base *cabeca_antiga=cabeca;
-	cabeca=&novo_no;
-	novo_no->proximo=cabeca_antiga;
+	no_base **cabeca_antiga=cabeca;
+	*cabeca=novo_no;
+	novo_no->proximo=*cabeca_antiga;
+}
+
+void print_list(no_base *cabeca){
+	no_base *no_atual=cabeca;
+	
+	while (no_atual!=NULL){
+		printf("%d",no_atual->valor);
+		no_atual=no_atual->proximo;
+	}
 }
 
 int main() {
-    
+    no_base *cabeca=NULL;
+    print_list(cabeca);
+    add_no(&cabeca,10);
+    print_list(cabeca);
     return 0;
 }
