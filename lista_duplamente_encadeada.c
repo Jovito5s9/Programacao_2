d92c3e5833d481a8cb04056cb2071f8a95c3231a
Jovito5s9 <sjovito70@gmail.com>
2025-11-24 20:19:13 -0300

    Correção na seleção da cabeça(sem mais loops infinitos) e melhor legibilidade da saída na função de printagem

--- lista_duplamente_encadeada.c
+++ lista_duplamente_encadeada.c
@@ -8,26 +8,29 @@ typedef struct no_base{
 	} no_base;
 
 void add_no(no_base **cabeca, int valor){
+	no_base **cabeca_antiga=cabeca;
 	no_base *novo_no = malloc(sizeof(no_base));
 	novo_no->valor=valor;
-	no_base **cabeca_antiga=cabeca;
-	*cabeca=novo_no;
 	novo_no->proximo=*cabeca_antiga;
+	novo_no->anterior=NULL;
+	*cabeca=novo_no;
 }
 
 void print_list(no_base *cabeca){
 	no_base *no_atual=cabeca;
-	
+	printf("\nNULL");
 	while (no_atual!=NULL){
-		printf("%d",no_atual->valor);
+		printf(" <-> [%d] <-> ",no_atual->valor);
 		no_atual=no_atual->proximo;
 	}
+	printf("NULL");
 }
 
 int main() {
     no_base *cabeca=NULL;
     print_list(cabeca);
     add_no(&cabeca,10);
+    add_no(&cabeca,11);
     print_list(cabeca);
     return 0;
 }
