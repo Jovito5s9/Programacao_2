212006bd3c9a45d3ac914094003384bd7eb24375
João Victor <jaovictor825@gmail.com>
2025-11-30 22:17:20 -0300

    ser ou ser, eis a questão. Se for pra ser, então o programa roda, se não, ele não roda

--- bst.c
+++ bst.c
@@ -36,43 +36,52 @@ Node *inserir(Node *raiz, int valor)
     return raiz;
 }
 
-Node* remover(Node* raiz, int valor) {
-    if (raiz == NULL) {
+Node *remover(Node *raiz, int valor)
+{
+    if (raiz == NULL)
+    {
         return NULL;
     }
 
-    if (valor < raiz->valor) {
+    if (valor < raiz->valor)
+    {
         raiz->esq = remover(raiz->esq, valor);
         return raiz;
     }
-    else if (valor > raiz->valor) {
+    else if (valor > raiz->valor)
+    {
         raiz->dir = remover(raiz->dir, valor);
         return raiz;
     }
 
-    //remoção de folhas
-    if (raiz->dir == NULL && raiz->esq == NULL) {
+    // remoção de folhas
+    if (raiz->dir == NULL && raiz->esq == NULL)
+    {
         free(raiz);
         return NULL;
     }
-    
-    //remoção de nó com 1 filho
-    if (raiz->esq == NULL) {
+
+    // remoção de nó com 1 filho
+    if (raiz->esq == NULL)
+    {
         Node *temp = raiz;
         raiz = raiz->dir;
         free(temp);
         return raiz;
     }
-    else if (raiz->dir == NULL) {
+    else if (raiz->dir == NULL)
+    {
         Node *temp = raiz;
         raiz = raiz->esq;
         free(temp);
         return raiz;
     }
-    //remoção de nó com 2 filhos
-    else {
+    // remoção de nó com 2 filhos
+    else
+    {
         Node *temp = raiz->dir;
-        while (temp->esq != NULL) {
+        while (temp->esq != NULL)
+        {
             temp = raiz->esq;
         }
 
@@ -95,6 +104,31 @@ void imprimirEmOrdem(Node *raiz)
 int main()
 {
     Node *raiz = NULL;
+
+    int resposta;
+    do
+    {
+        printf("Qual a operação que deseja realizar?\n");
+        printf("1. Inserir valor\n");
+        printf("2. Buscar valor\n");
+        printf("3. Remover valor\n");
+        printf("4. Percorrer árvore\n");
+        printf("0. Sair\n");
+        scanf("%d", &resposta);
+
+        switch (resposta)
+        {
+        case 1:
+            printf("Qual valor deseja inserir: ");
+            int valor;
+            scanf("%d", &valor);
+            inserir(raiz, valor);
+            break;
+
+        default:
+            break;
+        }
+    } while (resposta != 0);
 
     raiz = inserir(raiz, 50);
     inserir(raiz, 30);
