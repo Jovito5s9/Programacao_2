5c4ba12cd094a97768b657b396abe05a341effcf
Jovito5s9 <sjovito70@gmail.com>
2025-12-14 10:48:00 -0300

    adiçao dos metodos de criacao e rotaçao dos nos da arvore(para balanceamentos)

--- main.c
+++ main.c
@@ -16,9 +16,64 @@ void inicializarNIL(){
     NIL->esquerda=NIL->direita=NIL->pai=NULL;
 }
 
+RBNo* criarNo(int data){
+    RBNo *no = (RBNo *)malloc(sizeof(RBNo));
+    no->data=data;
+    no->cor='R';
+    no->esquerda=no->direita=no->pai=NIL;
+    return no;
+}
+
+void rotacionarEsq(RBNo *x){
+    RBNo *y;
+    y=x->direita;
+    x->direita=y->esquerda;
+    if (y->esquerda!= NIL){
+        y->esquerda->pai=x;
+    }
+    y->pai=x->pai;
+    if(x->pai==NIL){
+        raiz=y;
+    }
+    else if(x==x->pai->esquerda){
+        x->pai->esquerda=y;
+    }
+    else{
+        x->pai->direita=y;
+    }
+    y->esquerda=x;
+    x->pai=y;
+}
+
+void rotacionarDir(RBNo *x){
+    RBNo *y;
+    y=x->esquerda;
+    x->esquerda=y->direita;
+    if (y->direita!= NIL){
+        y->direita->pai=x;
+    }
+    y->pai=x->pai;
+    if(x->pai==NIL){
+        raiz=y;
+    }
+    else if(x==x->pai->direita){
+        x->pai->direita=y;
+    }
+    else{
+        x->pai->esquerda=y;
+    }
+    y->direita=x;
+    x->pai=y;
+}
+
+
+void corrigirInsercao(RBNo *z){
+
+}
+
 int main(){
     inicializarNIL();
     raiz=NIL;
-
+    criarNo(10);
     return 0;
 }
