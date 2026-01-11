fa397d5e14bee9fc54cd235259dbf7d691ccbe82
Jovito5s9 <sjovito70@gmail.com>
2026-01-11 15:17:04 -0300

    feat: função que convert para lower as palavaras

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #define ALFABETO_SIZE 26
 
@@ -15,12 +17,22 @@ NoTrie* CriarNo(){
     return NovoNo;
 }
 
-
+char* lower(char *palavra){
+    int limite = strlen(palavra);
+    char *palavra_lower = (char *)malloc((limite +1) * sizeof(char));
+    for(int i=0;i<=limite;i++){
+        palavra_lower[i] = tolower((unsigned char)palavra[i]);
+    }
+    return palavra_lower;
+}
 
 int main(){
     NoTrie *raiz = CriarNo();
     int i=10;
     char letra='a';
     printf("%c",i+letra);//base pra achar as letras
+    char *palavra = lower("TESTANDO ISSO");
+    printf("%s", palavra);
+    free(palavra);
     return 0;
 }
