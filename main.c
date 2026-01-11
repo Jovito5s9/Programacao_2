8d7a16a98a830fb2cb578bd66f4ad0168e389783
Jovito5s9 <sjovito70@gmail.com>
2026-01-11 14:47:32 -0300

    feat: criação de nó e ideia para achar as letra

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define ALFABETO_SIZE 26
 
@@ -8,6 +9,18 @@ typedef struct NoTrie{
     bool FimPalavra;
 }NoTrie;
 
+
+NoTrie* CriarNo(){
+    NoTrie *NovoNo = (NoTrie *)malloc(sizeof(NoTrie));
+    return NovoNo;
+}
+
+
+
 int main(){
+    NoTrie *raiz = CriarNo();
+    int i=10;
+    char letra='a';
+    printf("%c",i+letra);//base pra achar as letras
     return 0;
 }
